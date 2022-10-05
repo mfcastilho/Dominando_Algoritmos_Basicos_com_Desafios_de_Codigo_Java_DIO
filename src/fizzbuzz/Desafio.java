3397ab4675f4e858dc9769675f8d32c78d15d228
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-10-05 14:55:48 -0300

    Desafio4 Finalizado - Inserindo else no código

--- src/fizzbuzz/Desafio.java
+++ src/fizzbuzz/Desafio.java
@@ -14,6 +14,8 @@ public class Desafio {
             System.out.println("Fizz");
         }else if (N%5 == 0){
             System.out.println("Buzz");
+        }else{
+            System.out.println(N);
         }
     }
 }
